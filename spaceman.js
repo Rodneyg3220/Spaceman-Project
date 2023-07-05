40ca964fee8a3a415ac88a236bde431f1ba93829
Rodneyg3220 <rodneygivens9@gmail.com>
2023-07-05 02:03:30 -0400

    made update to project

---
+++ spaceman.js
@@ -0,0 +1,9 @@
+const mysteryWord = ['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 
+'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Miami Heat',
+'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'New York Knicks','Oklahoma City Thunder','Orlando Magic','Philadelphia 76ers','Phoenix Suns',
+'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz','Washington Wizards'];
+const startButton = document.querySelector('Start-Game');
+console.log('startButton');
+const resetButton = document.querySelector('Reset-Game')
+const wordChoice = [Math.floor(Math.random()*wordChoice.length)];
+const message
